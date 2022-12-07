147df251beae1accb4f2e9cf87f639f4a76e9f4d
Gmene00 <60399658+Gmene00@users.noreply.github.com>
2022-12-07 18:51:04 +0100

    Clustering y shapefiles

--- clustering.r
+++ clustering.r
@@ -1,12 +1,15 @@
 #Clustering
 rm(list=ls())
-
+library(cluster)
+library(factoextra)
+library(ggplot2)
 ######Idea:::: en vez de cada punto es un dia en una estacion, agruparlos por mes y hacer su media
 #por lo que despues cada punto indica mes y estacion, simplificamos el problema.
 
 ###############################################KMEANS
 meteo21<-read.csv("Meteo/Diario/meteoDiario21_byDay.csv",sep=";", dec=",")
 asocNombresNum<-read.csv("Meteo/estacion_distrito.csv",sep=";", dec=",")
+
 #Puesto que hay muchas estaciones de las cuales tenemos bastantes nulos, vamos a realizar distintas
 #aproximaciones dado el dataset que conseguimos del Ayuntamiento de Madrid
 
@@ -18,16 +21,14 @@ dfFirst<-subset(meteo21,meteo21$Estacion %in% vecEst)
 sapply(dfFirst, function(x) sum(is.na(x)))
 #Quitamos las magnitudes 87 y 88 ya que tienen muchos NA
 dfFirst<-dfFirst[,-c(10,11)]
-for(i in 6:ncol(dfFirst)) {
-  dfFirst[,i][is.na(dfFirst[,i])]<-median(dfFirst[,i],na.rm=TRUE)
-}
+
 
 #Ahora por estacion vamos a sacar la media de sus magnitudes.
 dfMeans<-NULL#dataframe con las medias de las magnitudes de cada estacion
 for(estacion in unique(dfFirst$Estacion)){
   estMean<-NULL
   a<-dfFirst[dfFirst$Estacion==estacion, ]
-  estMean<-colMeans(a[6:10])
+  estMean<-colMeans(a[6:10], na.rm=TRUE)
   dfMeans<-rbind(dfMeans,c(estacion,estMean))
 }
 dfMeans<-data.frame(dfMeans)
@@ -38,15 +39,15 @@ for(myrow in 1:nrow(dfMeans)){#añadimos el nombre a la estacion
   dfMeans[myrow,]$Nombre<-asocNombresNum[asocNombresNum$ESTACION==dfMeans[myrow,]$Estacion,]$NombreEstacion
 }
 
+
+#Kmeans
 #Una vez que tenemos rellenado el dataframe al completo escalamos los datos
 myKmeans<-data.frame(scale(dfMeans[3:7]))
-library(cluster)
-library(factoextra)
 #Ahora vamos a calcular el numero optimo de clusters para el kMeans
 #Para ello recurrimos al diagrama del codo.
 #fviz_nbclust(myKmeans, kmeans, method = "wss")#No funciona ya que se generan mas centros de clusters que filas.
 set.seed(123)
-#Establecemos 3 clusters
+#Establecemos 4 clusters
 kmeansResults<-kmeans(myKmeans, centers=4)
 #Inercia entre grupos, nos dice la varianza entre los grupos, cuanto mayor mejor es la clasificación. Valor a maximizar.
 kmeansResults$betweenss
@@ -55,34 +56,38 @@ kmeansResults$withinss
 #Inercia total intragrupos, es un valor a minimizar, cuanto menor es mejor.
 kmeansResults$tot.withinss
 
-
 estacion<-dfMeans$Nombre
-grupo<-kmeansResults$cluster
-datosRepresentacion<-data.frame(estacion, grupo)
+numEst<-dfMeans$Estacion
+grupoKmeans<-kmeansResults$cluster
+datosRepresentacion<-data.frame(estacion, numEst, grupoKmeans)
 
-library(ggplot2)
-#Se ve basatante mal.
 ggplot(datosRepresentacion)+
-  geom_point(mapping=aes(x=grupo, y=estacion), color=grupo, size=1)
+  geom_point(mapping=aes(x=grupoKmeans, y=estacion), color=grupoKmeans, size=1)
+#grupo 1 --> moratalaz
+#grupo 2 --> juancarlos y hortaleza, madrid este
+#grupo 3 --> estacion en plena m40
+#grupo 4 --> estaciones externas fuera de la m40 y situadas en parques
 
+
+#Hierarchical Clustering 
 dfMeansNames <- dfMeans[,-1]
 rownames(dfMeansNames) <- dfMeans[,1]
-distance <- get_dist(dfMeansNames[,2:6])#Distancia entre estaciones sin normalizar los datos
-fviz_dist(distance, gradient = list(low = "#00AFBB", mid = "white", high = "#FC4E07"))
-
 dd <- dist(dfMeansNames[,2:3], method = "euclidean")
 hc <- hclust(dd, method = "ward.D2")
 plot(hc,hang = -1, cex = 0.4)
-grp <- cutree(hc, k = 4)
-# Visualize
+grupoJerarquico <- cutree(hc, k = 4)
 plot(hc, cex = 0.6) # plot tree
 rect.hclust(hc, k = 4, border = 2:5) # add rectangle
-
+datosRepresentacion<-cbind(datosRepresentacion,grupoJerarquico)
 #En este dendograma y con la ayuda de la localizacion de las estaciones se ven relaciones
 #Rojo --> Madrid sur colindante con la m40
 #Verde --> Madrid exterior, fuera de la m40
 #Azul oscuro --> Juancarlos I, parque en el exterior, poco efecto isla calor
 #Azul claro --> Madrid norte, colindante a la m40
+
+#Exportamos lo sresultados del clustering
+write.csv2(datosRepresentacion, "Mapas/Clustering/aprox1.csv", row.names = FALSE)
+
 
 
 ###########################################FIN 1ª Aproximacion
@@ -102,16 +107,14 @@ dfSecond<-subset(meteo21,meteo21$Estacion %in% vecEst)
 #Quitamos las magnitudes que no nos interesan
 dfSecond<-dfSecond[,-c(6,7,10,11,12)]
 sapply(dfSecond, function(x) sum(is.na(x)))#1,7% de NA para la 83 y 2% para la 86 
-for(i in 6:ncol(dfSecond)) {
-  dfSecond[,i][is.na(dfSecond[,i])]<-median(dfSecond[,i],na.rm=TRUE)
-}
+
 
 #Ahora por estacion vamos a sacar la media de sus magnitudes.
 dfMeans<-NULL#dataframe con las medias de las magnitudes de cada estacion
 for(estacion in unique(dfSecond$Estacion)){
   estMean<-NULL
   a<-dfSecond[dfSecond$Estacion==estacion, ]
-  estMean<-colMeans(a[6:7])
+  estMean<-colMeans(a[6:7],na.rm=TRUE)
   dfMeans<-rbind(dfMeans,c(estacion,estMean))
 }
 dfMeans<-data.frame(dfMeans)
@@ -124,11 +127,103 @@ for(myrow in 1:nrow(dfMeans)){#añadimos el nombre a la estacion
 
 #Una vez que tenemos rellenado el dataframe al completo escalamos los datos
 myKmeans<-data.frame(scale(dfMeans[3:4]))
-library(cluster)
-library(factoextra)
+
 #Ahora vamos a calcular el numero optimo de clusters para el kMeans
 #Para ello recurrimos al diagrama del codo.
-fviz_nbclust(myKmeans, kmeans, method = "wss")#Recomendacion de 4 clusters
+fviz_nbclust(myKmeans, kmeans, method = "wss")#Recomendacion de 5 clusters
+set.seed(123)
+#Establecemos 5 clusters
+kmeansResults<-kmeans(myKmeans, centers=5)
+#Inercia entre grupos, nos dice la varianza entre los grupos, cuanto mayor mejor es la clasificación. Valor a maximizar.
+kmeansResults$betweenss
+#Inercia intragrupos, nos indica como de agrupados esta cada punto dentro de cada grupo, cuanto menor, mejor.
+kmeansResults$withinss
+#Inercia total intragrupos, es un valor a minimizar, cuanto menor es mejor.
+kmeansResults$tot.withinss
+
+estacion<-dfMeans$Nombre
+numEst<-dfMeans$Estacion
+grupoKmeans<-kmeansResults$cluster
+datosRepresentacion<-data.frame(estacion, numEst,grupoKmeans)
+
+ggplot(datosRepresentacion)+
+  geom_point(mapping=aes(x=grupoKmeans, y=estacion), color=grupoKmeans, size=1)
+#Grupo 1 --> parques de gran extension
+#Grupo 2 --> situados en parques cercanos o bien m30 o bien m40
+#Grupo 3 --> son principalmente estaciones situadas en zonas muy urbanas centro-sur, con poco efecto de zonas verdes a su alrededor, cercanas a industrias (madrid sur)
+#grupo 4 --> zona urbana norte
+
+
+
+#Hierarchical Clustering 
+dfMeansNames <- dfMeans[,-1]
+rownames(dfMeansNames) <- dfMeans[,1]
+dd <- dist(dfMeansNames[,2:3], method = "euclidean")
+hc <- hclust(dd, method = "ward.D2")
+plot(hc,hang = -1, cex = 0.5)
+grupoJerarquico <- cutree(hc, k = 5)
+plot(hc, cex = 0.6) # plot tree
+rect.hclust(hc, k = 5, border = 2:5) # add rectangle
+datosRepresentacion<-cbind(datosRepresentacion,grupoJerarquico)
+
+
+#Exportamos lo sresultados del clustering
+write.csv2(datosRepresentacion, "Mapas/Clustering/aprox2.csv", row.names = FALSE)
+#Conclusiones
+#Rojo --> son estaciones que se encuentran en zonas verdes, en las que la masa arborea es más elevada que en el resto de zonas.
+#verde --> chamberí, madrid urbana pero en la zona del norte.
+#Azul oscuro --> estaciones que se situan en nucleos urbanos entre la m30 y m40, en zonas urbanas
+#Azul claro --> se tratan de estaciones que están entre el centro y la m30, en zonas urbanas
+
+
+###############################################Fin 2ª aproximacion
+rm(list=ls())
+###3ª aproximacion, ahor avamos a tener en cuenta las zonas verdes de cada una de las estaciones/distrito
+#Estaciones con la magnitud 83 completa, y con datos sobre masas arboreas.
+#Estas son las 12 estaciones que cumplen los requisitos
+meteo21<-read.csv("Meteo/Diario/meteoDiario21_byDay.csv",sep=";", dec=",")
+asocNombresNum<-read.csv("Meteo/estacion_distrito.csv",sep=";", dec=",")
+vecEst<-c(18,24,36,38,54,58,59,102,103,106,107)
+sapply(meteo21, function(x) sum(is.na(x)))
+dfThird<-subset(meteo21,meteo21$Estacion %in% vecEst)
+#Quitamos las magnitudes que no nos interesan
+dfThird<-dfThird[,-c(6,7,9,10,11,12)]
+sapply(dfThird, function(x) sum(is.na(x)))#0,7% de NA para la 83  
+
+#Ahora por estacion vamos a sacar la media de sus magnitudes.
+dfMeans<-NULL#dataframe con las medias de las magnitudes de cada estacion
+for(estacion in unique(dfThird$Estacion)){
+  estMean<-NULL
+  a<-dfThird[dfThird$Estacion==estacion, ]
+  estMean<-colMeans(a[6], na.rm=TRUE)
+  dfMeans<-rbind(dfMeans,c(estacion,estMean))
+}
+dfMeans<-data.frame(dfMeans)
+names(dfMeans)[1] <- 'Estacion'
+Nombre<-rep(NA,11)
+dfMeans<-cbind(Nombre,dfMeans)
+for(myrow in 1:nrow(dfMeans)){#añadimos el nombre a la estacion
+  dfMeans[myrow,]$Nombre<-asocNombresNum[asocNombresNum$ESTACION==dfMeans[myrow,]$Estacion,]$NombreEstacion
+}
+
+
+zonasVerdes<-read.csv("Zonas Verdes/masaArborea21.csv",sep=";", dec=",")
+#Rellenamos con los datos sobre superficie arborea
+superficie<-rep(NA,11)
+dfMeans<-cbind(dfMeans,superficie)
+for(myrow in 1:nrow(dfMeans)){
+  mydistr<-asocNombresNum[asocNombresNum$ESTACION==dfMeans[myrow,]$Estacion,]$DISTRITO
+  dfMeans[myrow,]$superficie<-zonasVerdes[zonasVerdes$NumDistr==mydistr,]$Superficiem2
+}
+
+
+#KMeans
+#Una vez que tenemos rellenado el dataframe al completo escalamos los datos
+myKmeans<-data.frame(scale(dfMeans[3:4]))
+
+#Ahora vamos a calcular el numero optimo de clusters para el kMeans
+#Para ello recurrimos al diagrama del codo.
+fviz_nbclust(myKmeans, kmeans, method = "wss")#Recomendacion de 3 clusters
 set.seed(123)
 #Establecemos 3 clusters
 kmeansResults<-kmeans(myKmeans, centers=4)
@@ -139,57 +234,36 @@ kmeansResults$withinss
 #Inercia total intragrupos, es un valor a minimizar, cuanto menor es mejor.
 kmeansResults$tot.withinss
 
-
 estacion<-dfMeans$Nombre
-grupo<-kmeansResults$cluster
-datosRepresentacion<-data.frame(estacion, grupo)
+numEst<-dfMeans$Estacion
+grupoKmeans<-kmeansResults$cluster
+datosRepresentacion<-data.frame(estacion, numEst, grupoKmeans)
 
-library(ggplot2)
-#Se ve basatante mal.
 ggplot(datosRepresentacion)+
-  geom_point(mapping=aes(x=grupo, y=estacion), color=grupo, size=1)
+  geom_point(mapping=aes(x=grupoKmeans, y=estacion), color=grupoKmeans, size=1)
+#Grupo 1 --> proximos a zonas verdes, pero pegados a circunvalaciones, madird oeste-norte
+#Grupo 2 --> parques de menor estension, madrid este-sur, cuatro caminos me chirria
+#Grupo 3 --> zonas exteriores a las circunvalaciones, pero en zonas urbanas
+#Grupo 4 --> zonas verdes, exteriores, no nucleo madrid
 
+#Jerarquico
 dfMeansNames <- dfMeans[,-1]
 rownames(dfMeansNames) <- dfMeans[,1]
-distance <- get_dist(dfMeansNames[,2:3])#Distancia entre estaciones sin normalizar los datos
-fviz_dist(distance, gradient = list(low = "#00AFBB", mid = "white", high = "#FC4E07"))
-
 dd <- dist(dfMeansNames[,2:3], method = "euclidean")
 hc <- hclust(dd, method = "ward.D2")
 plot(hc,hang = -1, cex = 0.4)
-grp <- cutree(hc, k = 4)
-# Visualize
+grupoJerarquico <- cutree(hc, k = 4)
 plot(hc, cex = 0.6) # plot tree
 rect.hclust(hc, k = 4, border = 2:5) # add rectangle
-
-#Conclusiones
-#Rojo --> son estaciones que se encuentran en zonas verdes, en las que la masa arborea es más elevada que en el resto de zonas.
-#verde --> chamberí, madrid ciudad pero en la zona del norte.
-#Azul oscuro --> estaciones que se situan en nucleos urbanos entre la m30 y m40
-#Azul claro --> se tratan de estaciones que están entre el centro y la m30
-
+datosRepresentacion<-cbind(datosRepresentacion,grupoJerarquico)
+#Grupo 1 --> madrid norte cuatro caminos y este, zonas urbanas
+#Grupo 2 --> zonax esteriores circunvalaciones
+#Grupo 3 --> grandes zonas verdes, pero proximas a circunvalaciones
+#grupo 4 --> zonas verdes de madrid norte
 
 
-###############################################Fin 2ª aproximacion
-rm(list=ls())
-###3ª aproximacion, ahor avamos a tener en cuenta las zonas verdes de cada una de las estaciones/distrito
-###Lo malo------> asigno la misma zona verde a las distintas estaciones del mismo distrito?
+#Exportamos lo sresultados del clustering
+write.csv2(datosRepresentacion, "Mapas/Clustering/aprox3.csv", row.names = FALSE)
 
 
 
-###########################################DECISION TREE
-#https://www.geeksforgeeks.org/decision-tree-in-r-programming/?ref=lbp
-library(dplyr)
-library(caTools)
-library(party)
-library(magrittr)
-sample_data = sample.split(dfMeans[,3:9], SplitRatio = 0.8)
-train_data <- subset(dfMeans[,3:9], sample_data == TRUE)
-test_data <- subset(dfMeans[,3:9], sample_data == FALSE)
-model<- ctree(X83 ~ .,dfMeans[,3:9])
-plot(model)
-
-
-predict_model<-predict(model, test_data)
-m_at <- table(test_data$X83, predict_model)
-m_at
---
+++ mapas.r
@@ -0,0 +1,98 @@
+#install.packages(c("rgeos", "sp", "maptools", "RColorBrewer", "classInt", "rgdal", "ggmap", "googleVis"))
+rm(list=ls())
+library(rgeos)
+library(maptools)
+library(sp)
+library(ggmap)
+library(rgdal)
+library(readxl)
+
+estacionesControl <- read_excel("Meteo/Estaciones_control_datos_meteorologicos.xls")
+estCon<-estacionesControl[,c(2,3, 20, 21)]
+estCon$COORDENADA_X_ETRS89<-as.numeric(estCon$COORDENADA_X_ETRS89)
+estCon$COORDENADA_Y_ETRS89<-as.numeric(estCon$COORDENADA_Y_ETRS89)
+aprox1 <- read.csv("Mapas/Clustering/aprox1.csv",sep=";")
+aprox2 <- read.csv("Mapas/Clustering/aprox2.csv",sep=";")
+aprox3 <- read.csv("Mapas/Clustering/aprox3.csv",sep=";")
+x<-rep(NA,8)
+y<-rep(NA,8)
+aprox1<-cbind(aprox1, x, y)
+for(myrow in 1:nrow(aprox1)){#rellenamos coordenadas
+  myx<-estCon[estCon$CÓDIGO_CORTO==aprox1[myrow,]$numEst,]$COORDENADA_X_ETRS89
+  myy<-estCon[estCon$CÓDIGO_CORTO==aprox1[myrow,]$numEst,]$COORDENADA_Y_ETRS89
+  aprox1[myrow,]$x<-myx
+  aprox1[myrow,]$y<-myy
+}
+
+x<-rep(NA,16)
+y<-rep(NA,16)
+aprox2<-cbind(aprox2, x, y)
+for(myrow in 1:nrow(aprox2)){#rellenamos coordenadas
+  myx<-estCon[estCon$CÓDIGO_CORTO==aprox2[myrow,]$numEst,]$COORDENADA_X_ETRS89
+  myy<-estCon[estCon$CÓDIGO_CORTO==aprox2[myrow,]$numEst,]$COORDENADA_Y_ETRS89
+  aprox2[myrow,]$x<-myx
+  aprox2[myrow,]$y<-myy
+}
+
+x<-rep(NA,11)
+y<-rep(NA,11)
+aprox3<-cbind(aprox3, x, y)
+for(myrow in 1:nrow(aprox3)){#rellenamos coordenadas
+  myx<-estCon[estCon$CÓDIGO_CORTO==aprox3[myrow,]$numEst,]$COORDENADA_X_ETRS89
+  myy<-estCon[estCon$CÓDIGO_CORTO==aprox3[myrow,]$numEst,]$COORDENADA_Y_ETRS89
+  aprox3[myrow,]$x<-myx
+  aprox3[myrow,]$y<-myy
+}
+
+
+
+getinfo.shape("Mapas/Barrios/200001909.shp")
+
+ED50 <- CRS(paste("+proj=utm +zone=30 +ellps=intl +units=m +no_defs"))
+barriosMadrid <- readShapePoly("Mapas/Barrios/200001909.shp",proj4string = ED50)
+str(barriosMadrid, max.level = 2)
+head(coordinates(barriosMadrid))
+barriosMadrid@bbox
+head(barriosMadrid@data)
+plot(barriosMadrid)
+
+#Enseña barrios en vez de distritos
+#distritosMadrid <- readShapePoly("Mapas/Barrios/200001909.shp",proj4string = ED50)
+#plot(distritosMadrid)
+
+
+#Visualizacion de los resultados de la aproximacion 1
+coordinates(aprox1) <- c("x","y")
+proj4string(aprox1)<-ED50
+
+plot(barriosMadrid)
+plot(aprox1, pch=20, cex=2,col=aprox1$grupoKmeans, add=TRUE)
+
+plot(barriosMadrid)
+plot(aprox1, pch=20, cex=2,col=aprox1$grupoJerarquico, add=TRUE)
+
+
+#Visualizacion de los resultados de la aproximacion 2
+coordinates(aprox2) <- c("x","y")
+proj4string(aprox2)<-ED50
+
+plot(barriosMadrid)
+plot(aprox2, pch=20, cex=2,col=aprox2$grupoKmeans, add=TRUE)
+
+plot(barriosMadrid)
+plot(aprox2, pch=20, cex=2,col=aprox2$grupoJerarquico, add=TRUE)
+
+
+#Visualizacion de los resultados de la aproximacion 2
+coordinates(aprox3) <- c("x","y")
+proj4string(aprox3)<-ED50
+
+plot(barriosMadrid)
+plot(aprox3, pch=20, cex=2,col=aprox3$grupoKmeans, add=TRUE)
+
+plot(barriosMadrid)
+plot(aprox3, pch=20, cex=2,col=aprox3$grupoJerarquico, add=TRUE)
+
+
+
+
